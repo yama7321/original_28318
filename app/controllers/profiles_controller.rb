57c866d62d42b21f82ce2dd93cdc45c77cc3da25
yama7321 <ryota.yamaoka7321@gmail.com>
2020-08-19 21:28:52 +0900

    メンター一覧機能の実装

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,25 @@
 class ProfilesController < ApplicationController
   def index
-    
+    @profiles = Profile.all.order('created_at DESC').page(params[:page]).per(4)
+  end
+
+  def new
+    @profile = Profile.new
+  end
+
+  def create
+    @profile = Profile.new(profile_params)
+    if @profile.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
   
+  private
+  def profile_params
+    params.require(:profile).permit(:title, :image, :slide, :self_introduction, :price).merge(user_id: current_user.id)
+  end
+
+
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,32 @@
 class Profile < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+  has_one_attached :slide
+
+  with_options presence: true do
+    validates :title
+    validates :self_introduction
+    validates :price
+  end
+
+  validate :image_presence
+  validate :slide_presence
+
+  def image_presence
+    if image.attached?
+      errors.add(:image, 'にはjpegまたはpngファイルを添付してください') unless image.content_type.in?(%('image/jpeg image/png'))
+    else
+      errors.add(:image, 'ファイルを添付してください')
+    end
+  end
+
+  def slide_presence
+    if slide.attached?
+      errors.add(:slide, 'にはjpegまたはpngファイルを添付してください') unless image.content_type.in?(%('image/jpeg image/png'))
+    else
+      errors.add(:slide, 'ファイルを添付してください')
+    end
+  end
+
+  
 end
