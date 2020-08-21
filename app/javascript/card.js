2a93d40841484ef5b743960006f06b21aad17655
yama7321 <ryota.yamaoka7321@gmail.com>
2020-08-21 17:16:27 +0900

    マイページ機能の実装

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -30,7 +30,7 @@ const pay = () => {
         document.getElementById("charge-form").submit();
         document.getElementById("charge-form").reset();
       } else {
-        alert('カード情報が間違っています')
+        alert('カード情報が間違っています');
         location.reload();
       }
     });
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'profiles#index'
   devise_for :users
+  resources :users, only: [:show, :edit, :update, :destroy]
   resources :profiles do
     resources :orders, only: [:index, :create]
   end
