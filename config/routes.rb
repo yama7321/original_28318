021c77daaa7420ac6db30f4feabc621f75d55abe
yama7321 <ryota.yamaoka7321@gmail.com>
2020-08-26 21:20:42 +0900

    ソート機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'messages/index'
   root 'profiles#index'
+  post '/', to: 'profiles#index'
   devise_for :users
   resources :users, only: [:show, :edit, :update, :destroy]
   resources :profiles do
