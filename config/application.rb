fc5c5041735cc039d8499be16fa047ae278f551e
yama7321 <ryota.yamaoka7321@gmail.com>
2020-09-04 09:39:03 +0900

    メッセージのバリデーションによるビュー崩れ修正

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,7 @@ module Original28318
     # the framework and any gems in your application.
     config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag } 
 
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
