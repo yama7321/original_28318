d670525ae54851f964ea2054bf5cdad61a81d377
yama7321 <ryota.yamaoka7321@gmail.com>
2020-09-04 11:49:01 +0900

    レビュー機能単体テスト実装

--- spec/factories/reviews.rb
+++ spec/factories/reviews.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :review do
-    
+    content {Faker::Lorem.sentence}
+    valuation {5}
+    association :user
+    association :profile
   end
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#create" do
+    before do
+      @review = FactoryBot.build(:review)
+    end
+
+    it "contentが存在していれば保存できること" do
+      expect(@review).to be_valid
+    end
+    
+    it "contentが空だと保存できないこと" do
+      @review.content = nil
+      @review.valid?
+      expect(@review.errors.full_messages).to  include("Content can't be blank")
+    end
+    
+  end
+  
 end
