1ccfe78c6e5f42cdd8981097c61bed45797f32b3
yama7321 <ryota.yamaoka7321@gmail.com>
2020-10-09 15:35:26 +0900

    ルーティングファイルの可読性向上

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,20 @@
 Rails.application.routes.draw do
-  get 'messages/index'
+  # トップページはメンターのプロフィールを一覧表示
   root 'profiles#index'
   post '/', to: 'profiles#index'
-  devise_for :users
-  resources :users, only: [:show, :edit, :update, :destroy]
+
   resources :profiles do
+    # メンターのプロフィールを購入することでメンター契約ができる
     resources :orders, only: [:index, :create, :destroy]
+    # メンターのプロフィールに対してレビューができる
     resources :reviews, only: [:create, :destroy]
   end
+
+  devise_for :users
+  resources :users, only: [:show, :edit, :update, :destroy]
+
+  # メッセージ画面
+  get 'messages/index'
   resources :rooms do
     resources :messages
   end
