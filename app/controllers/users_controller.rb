f6a489d73c48e0efd86a519a37382b22babc2657
yama7321 <ryota.yamaoka7321@gmail.com>
2020-10-09 15:37:18 +0900

    コメントを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   
   def show
+    # 現在ログインしているユーザーの契約情報を@ordersに格納
     @orders = Order.where(user_id: current_user.id).order('created_at DESC')
   end
   
